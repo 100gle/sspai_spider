85ca7634cf8ebd99041fd394174c718ffd3739b0
100gle <loogle.space@gmail.com>
2019-09-26 10:06:11 +0800

    [DONE] user info get

--- sspai_user_info.py
+++ sspai_user_info.py
@@ -1,26 +1,30 @@
 import requests
-import pprint
 import json
-from bs4 import BeautifulSoup
+from pprint import pprint
+from requests.exceptions import RequestException
 
-#TODO: api need to be changes
-base = 'https://beta.sspai.com/api/v1/user/slug/info/get?slug=100gle'
+def user_info(slug):
+    base = 'https://beta.sspai.com/api/v1/user/slug/info/get?slug={}'.format(str(slug))
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36', 
+        'Referer': 'https://beta.sspai.com/u/{}/updates'.format(str(slug))}
+    try: 
+        res = requests.get(base, headers= headers)
+        json_data = json.loads(res.text)
 
+        user_info = {}
+        user_info['view_count'] = json_data['data']['article_view_count']
+        user_info['word_count'] = json_data['data']['articles_word_count'] 
+        user_info['like_count'] = json_data['data']['liked_count']
+        user_info['create_at'] = json_data['data']['created_at']
+        user_info['followers_num'] = json_data['data']['followed_count']        
+        pprint(user_info)
 
-#TODO: Referer need to be changes
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36', 
-    'Referer': 'https://beta.sspai.com/u/100gle/updates'}
+        return user_info
 
-res = requests.get(base, headers= headers)
+    except RequestException as e:
+        print('unable to get page content: %s' %e)
 
-json_data = json.loads(res.text)
-pprint.pprint(json_data)
-
-#TODO: user infos
-view_count = json_data['data']['article_view_count']
-word_count = json_data['data']['articles_word_count']
-like_count = json_data['data']['liked_count']
-create_at = json_data['data']['created_at']
-followers_num = json_data['data']['followed_count']
-
+if __name__ == "__main__":
+    slug='fhwl2s3u'
+    user_info(slug)
